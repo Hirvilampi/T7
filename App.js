92d96773dd8545f22e97c89d5c89585baeaca42f
Timo Lampinen <timolampinen@unknown26af98da6118.elisakoti>
2025-09-14 20:03:58 +0300

    donkattu

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, Button, SafeAreaView,
+  ActivityIndicator, TextInput, FlatList, Image
+} from 'react-native';
+import { useState, useEffect } from 'react';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
+  const [keyword, setKeyword] = useState(0);
+  const [currencies, setCurrencies] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [currency, setCurrency] = useState("USD");
+  const [value, setValue] = useState(0);
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'apikey': 'SPHc3GEDFW5DnDzDy8tLqKnJKmDk47Ay'
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.apilayer.com/exchangerates_data/latest?base=EUR`, requestOptions)
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error in fetch:" + response.statusText);
+        return response.json()
+      })
+      .then(data => setCurrencies(data.rates ?? {}))
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  }, []);
+//  console.log(currencies);
+
+  const convert = () => {
+    const numeric = parseFloat(keyword);
+    const newrate = numeric * currencies[currency]
+    setValue(newrate.toFixed(2));
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize: 24}}>{value} €</Text>
+        {loading && <ActivityIndicator size="large" />} 
+
+      <View style={{paddingTop: 20, textDecorationLine: 'underline', flexDirection: 'row', marginLeft:100,}}>
+        <TextInput
+          style={{ fontSize: 18, borderRadius: 6 }}
+          placeholder='Amount'
+          keyboardType="decimal-pad"
+          value={keyword}
+          onChangeText={text => setKeyword(text)}
+        />
+
+        <Picker
+          selectedValue={currency}
+          onValueChange={(itemValue) => setCurrency(itemValue)}
+          style={styles.picker}
+          mode='dropdown'
+        >
+          {Object.keys(currencies).map((cur) => (
+            <Picker.Item key={cur} label={cur} value={cur} />
+          ))}
+        </Picker>
+      </View>
+
+      <View>
+        <Button title="CONVERT" onPress={convert} />
+      </View>
+
+      <StatusBar />
+    </SafeAreaView>
+
   );
 }
 
@@ -16,5 +82,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  picker: {
+    height: 200,
+    width: 140,
+    marginRight: 100,
   },
 });
